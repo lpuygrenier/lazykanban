877c88b259e098b96b301a1441f28f8ef05a0813
leo.puygrenier <leo.puygrenier@gmail.com>
2025-09-12 16:36:10 +0200

    txt editor

--- src/Gui/Component/Input.php
+++ src/Gui/Component/Input.php
@@ -7,6 +7,7 @@ namespace Lpuygrenier\Lazykanban\Gui\Component;
 use Lpuygrenier\Lazykanban\Entity\Board;
 use Lpuygrenier\Lazykanban\Entity\Status;
 use Lpuygrenier\Lazykanban\Entity\Task;
+use Lpuygrenier\Lazykanban\Gui\Component\TextArea\TextEditor;
 use Lpuygrenier\Lazykanban\Gui\KeyboardAction;
 use Monolog\Logger;
 use PhpTui\Term\Event;
@@ -40,33 +41,47 @@ final class Input implements GuiComponent
 
     private $placeholder = "";
     private $label = "label";
-    private $text = " ";
-    private $cursor = 0;
+    private TextEditor $textEditor;
 
-    private Logger $logger;
+    private ?Logger $logger;
 
-    public function __construct(Logger $logger)
+    public function __construct(?Logger $logger = null)
     {
         $this->logger = $logger;
+        $this->textEditor = TextEditor::fromString("");
     }
 
     public function build(): Widget
     {
-        $before = substr($this->text, 0, $this->cursor);
-        $after = substr($this->text, $this->cursor);
-        $cursorChar = $this->cursor < strlen($this->text) ? substr($this->text, $this->cursor, 1) : '_';
-        $line = Line::fromSpans(
-            Span::fromString($before),
-                   Span::fromString($cursorChar)->style(Style::default()->bg(RgbColor::fromRgb(255, 255, 255))->fg(color: RgbColor::fromRgb(0, 0, 0))),
-
-            Span::fromString($after),
-        );
+        $lines = $this->textEditor->viewportLines(0, $this->textEditor->lineCount());
+        $cursorPos = $this->textEditor->cursorPosition();
+        $displayLines = [];
+        foreach ($lines as $index => $line) {
+            if ($index === $cursorPos->y) {
+                if ($line === "") {
+                    $displayLines[] = Line::fromSpans(
+                        Span::fromString("_")->style(Style::default()->bg(RgbColor::fromRgb(255, 255, 255))->fg(RgbColor::fromRgb(0, 0, 0))),
+                    );
+                } else {
+                    $before = mb_substr($line, 0, $cursorPos->x);
+                    $after = mb_substr($line, $cursorPos->x);
+                    $cursorChar = $cursorPos->x < mb_strlen($line) ? mb_substr($line, $cursorPos->x, 1) : '_';
+                    $displayLines[] = Line::fromSpans(
+                        Span::fromString($before),
+                        Span::fromString($cursorChar)->style(Style::default()->bg(RgbColor::fromRgb(255, 255, 255))->fg(RgbColor::fromRgb(0, 0, 0))),
+                        Span::fromString($after),
+                    );
+                }
+            } else {
+                $displayLines[] = Line::fromString($line ?: " ");
+            }
+        }
         return BlockWidget::default()
             ->borders(Borders::ALL)
             ->titles(Title::fromString($this->label))
             ->widget(
                 ParagraphWidget::fromText(
-                    Text::fromLines($line)
+                    Text::fromLines(...$displayLines)
                 )->wrap(Wrap::Word)
             );
     }
@@ -75,25 +90,27 @@ final class Input implements GuiComponent
     public function handleKeybindAction(KeyboardAction $keyboardAction): void {
         $event = $keyboardAction->getEvent();
 
-        $this->logger->info( $event->__tostring());
+        if ($this->logger) {
+            $this->logger->info($event->__toString());
+        }
 
         if ($event instanceof CharKeyEvent) {
-            $this->text = substr($this->text, 0, $this->cursor) . $event->char . substr($this->text, $this->cursor);
-            $this->cursor++;
+            $this->textEditor->insert($event->char);
         } elseif ($event instanceof CodedKeyEvent) {
             if ($event->code === KeyCode::Backspace) {
-                if ($this->cursor > 0) {
-                    $this->text = substr($this->text, 0, $this->cursor - 1) . substr($this->text, $this->cursor);
-                    $this->cursor--;
-                }
+                $this->textEditor->deleteBackwards();
+            } elseif ($event->code === KeyCode::Delete) {
+                $this->textEditor->delete();
+            } elseif ($event->code === KeyCode::Enter) {
+                $this->textEditor->newLine();
             } elseif ($event->code === KeyCode::Left) {
-                if ($this->cursor > 0) {
-                    $this->cursor--;
-                }
+                $this->textEditor->cursorLeft();
             } elseif ($event->code === KeyCode::Right) {
-                if ($this->cursor < strlen($this->text)) {
-                    $this->cursor++;
-                }
+                $this->textEditor->cursorRight();
+            } elseif ($event->code === KeyCode::Up) {
+                $this->textEditor->cursorUp();
+            } elseif ($event->code === KeyCode::Down) {
+                $this->textEditor->cursorDown();
             }
         }
     }
